77d8938706eb7042072b13ed6a28351b59cd71e4
ArkBriar <arkbriar@gmail.com>
2015-11-23 13:01:20 +0800

    update symlink g++

--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -9,12 +9,17 @@ wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.2-0qt5_essentials.7z.sha1
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.2-0icu_53_1_ubuntu_11_10_64.7z
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.2-0icu_53_1_ubuntu_11_10_64.7z.sha1
+wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.2-0qt5_addons.7z
+wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.2-0qt5_addons.7z.sha1
 
 7z x 5.4.2-0qt5_essentials.7z > /dev/null
 7z x 5.4.2-0icu_53_1_ubuntu_11_10_64.7z > /dev/null
+7z x 5.4.2-0qt5_addons.7z > /dev/null
 
 export CC="gcc-4.8"
 export CXX="g++-4.8"
 export PATH=$PWD/5.5/gcc_64/bin:$PATH
+
+sudo update-alternatives --install $(which g++) g++ $(which g++-4.8) 100
 
 popd
